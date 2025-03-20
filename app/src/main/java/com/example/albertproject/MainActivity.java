7c24509d3003f871b7c567a3a6459a96fbde5113
gs7073 <gs7073@bs.amalnet.k12.il>
2025-03-20 11:53:35 +0200

    EditText+Button

--- app/src/main/java/com/example/albertproject/MainActivity.java
+++ app/src/main/java/com/example/albertproject/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.albertproject;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView, textView2;
+    EditText et1;
+    Button btn1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et1 = findViewById(R.id.et1);
+        btn1 = findViewById(R.id.btn1);
 
         textView = findViewById(R.id.textView);
         textView2 = findViewById(R.id.textView2);
@@ -21,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         Random rnd = new Random();
         int random1 = rnd.nextInt(89) + 10;
         int random2 = rnd.nextInt(89) + 10;
-
+        int correct_answer = random1+random2;
         textView.setText(String.valueOf(random1));
         textView2.setText(String.valueOf(random2));
     }
