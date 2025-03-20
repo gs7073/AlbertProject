6ece82ad99921d10b35ed225fa01788ad800831d
gs7073 <gs7073@bs.amalnet.k12.il>
2025-03-20 13:36:57 +0200

    Design + first column

--- app/src/main/java/com/example/albertproject/MainActivity.java
+++ app/src/main/java/com/example/albertproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.albertproject;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView, textView2;
     EditText et1;
     Button btn1;
+    ImageView image_result;
+
 
 
     @Override
@@ -21,9 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         et1 = findViewById(R.id.et1);
         btn1 = findViewById(R.id.btn1);
+        image_result = findViewById(R.id.image_result2);
 
-        textView = findViewById(R.id.textView);
-        textView2 = findViewById(R.id.textView2);
+        textView = findViewById(R.id.textView3);
+        textView2 = findViewById(R.id.textView4);
 
         Random rnd = new Random();
         int random1 = rnd.nextInt(89) + 10;
@@ -31,5 +36,24 @@ public class MainActivity extends AppCompatActivity {
         int correct_answer = random1+random2;
         textView.setText(String.valueOf(random1));
         textView2.setText(String.valueOf(random2));
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String answerText = et1.getText().toString();
+                if (!answerText.isEmpty()) {
+                    int answer = Integer.parseInt(answerText);
+                    if (answer == correct_answer) {
+                        image_result.setImageResource(R.drawable.v);
+                    } else {
+                        image_result.setImageResource(R.drawable.x);
+                    }
+
+                    image_result.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
 }
